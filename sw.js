d8501f833c02c04643bd1cebad02626279cc2391
adnanali9262 <adnan.rafiq173@GMAIL.COM>
2025-11-11 11:49:12 +0500

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -9,18 +9,12 @@ const ASSETS = [
   '/calculators/dc-cable.html',
   '/calculators/energy-units.html'
 ];
-// install
+
 self.addEventListener('install', evt => {
   evt.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-// activate
-self.addEventListener('activate', evt => {
-  evt.waitUntil(self.clients.claim());
-});
-// fetch
+self.addEventListener('activate', evt => evt.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(res => res || fetch(evt.request).catch(()=>caches.match('/index.html')))
-  );
+  evt.respondWith(caches.match(evt.request).then(res => res || fetch(evt.request).catch(()=>caches.match('/index.html'))));
 });
