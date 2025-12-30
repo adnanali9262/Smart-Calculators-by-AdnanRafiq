c09cab01156f6858e6cbda1ca2a94494d61cdbab
adnanali9262 <adnan.rafiq173@gmail.com>
2025-12-30 12:23:43 +0500

    Your commit message

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
 const CACHE = 'smart-calculators-v1';
+
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  '/calculators.json',
-  '/calculators/dc-cable.html',
-  '/calculators/energy-units.html'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './calculators.json',
+  './calculators/dc-cable.html',
+  './calculators/energy-units.html'
 ];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
-self.addEventListener('activate', evt => evt.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', evt => {
-  evt.respondWith(caches.match(evt.request).then(res => res || fetch(evt.request).catch(()=>caches.match('/index.html'))));
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request).catch(() => caches.match('./index.html'))
+    )
+  );
 });
